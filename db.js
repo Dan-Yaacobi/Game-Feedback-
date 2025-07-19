76aaf6270681cd480e0025b7b246369da7b1bf60
Dan-Yaacobi <yaacobidan@gmail.com>
2025-07-19 17:29:22 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'bugtracker',
-  password: '#######',
+  database: 'postgres',
+  password: process.env.DB_PASSWORD,
   port: 5432
 })
 
